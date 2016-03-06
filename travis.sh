c888f474ca79585cdd8ec3e9304b263c7e7f60a4
Daniel Heráclio <dheraclio@gmail.com>
2016-03-06 20:54:04 -0300

    Adding dependency not found in maven central

--- travis.sh
+++ travis.sh
@@ -1,2 +1,5 @@
+#Install dependency not found in maven central
+mvn install:install-file -Dfile=dependencies/org.eclipse.cdt.core-5.3.1.201109151620.jar -DpomFile=dependencies/org.eclipse.cdt.core-5.3.1.201109151620.pom
+
 #Run test in all modules
 mvn clean test
